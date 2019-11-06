c14bc3f28b81d577542557408eaad22c97b30476
hotroit <hotroit@vttek.vn>
2019-11-06 09:29:38 +0700

    sua bug jsonQT

--- database.php
+++ database.php
@@ -1,20 +1,23 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 //$dsn = 'mysql:host=localhost;dbname=thongke';
 //$username = 'root';
 //$password = '';
 
 $dsn1 = 'mysql:host=localhost;dbname=Report';
-$dsn2 = 'mysql:host=localhost;dbname=VHTReport';
+//$dsn2 = 'mysql:host=localhost;dbname=VHTReport';
 $username = 'root';
-$password = 'Report@VHT2019';
+$password = '';
 
 try {
 $db = new PDO($dsn1, $username, $password);
         $db->exec("SET NAMES 'utf8';");
-$dbVHT = new PDO($dsn2, $username, $password);
-        $dbVHT->exec("SET NAMES 'utf8';");
+//$dbVHT = new PDO($dsn2, $username, $password);
+        //$dbVHT->exec("SET NAMES 'utf8';");
 } catch (PDOException $e) {
-$error_message = $e->getMessage();
-exit();
+        $error_message = $e->getMessage();
+        echo $error_message;
+        exit();
 }
-?>
--- json/jsonQT.php
+++ json/jsonQT.php
@@ -4,26 +4,26 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once('../database.php');
+
 $quy_trinh_id = $_POST['data']['quy_trinh'] ?? null;
 $du_an = $_POST['data']['du_an'] ?? null;
 $don_vi = $_POST['data']['don_vi'] ?? null;
-if( !empty($quy_trinh_id) && !empty($du_an)){
-    $ma_du_an='('.implode( ",", $du_an ).')';
-    $query="SELECT du_an.ten, kpi_quytrinh.diem, kpi_quytrinh.thoigian
+if (!empty($quy_trinh_id) && !empty($du_an)) {
+    $ma_du_an = '(' . implode(",", $du_an) . ')';
+    $query = "SELECT du_an.ten, kpi_quytrinh.diem, kpi_quytrinh.thoigian
     FROM kpi_quytrinh
     Inner join du_an on du_an.id = kpi_quytrinh.du_an_id
     WHERE quy_trinh_id = $quy_trinh_id AND du_an_id IN $ma_du_an Order by thoigian";
 
     $statement = $db->prepare($query);
     $statement->execute();
-    if($statement->rowCount() >= 1) $kpi = $statement->fetchAll(PDO::FETCH_ASSOC|PDO::FETCH_GROUP);
+    if ($statement->rowCount() >= 1) $kpi = $statement->fetchAll(PDO::FETCH_ASSOC | PDO::FETCH_GROUP);
     $statement->closeCursor();
-    $myJSON = json_encode($kpi ?? '',JSON_UNESCAPED_UNICODE);
+    $myJSON = json_encode($kpi ?? '', JSON_UNESCAPED_UNICODE);
     echo $myJSON;
-}
-else if (!empty($don_vi)){
-    $don_vi = '('.implode( ",", $don_vi ).')';
-    $query="SELECT don_vi.ten, ROUND(AVG(diem)) as diem, thoigian 
+} else if (!empty($don_vi)) {
+    $don_vi = '(' . implode(",", $don_vi) . ')';
+    $query = "SELECT don_vi.ten, ROUND(AVG(diem)) as diem, thoigian 
     FROM kpi_quytrinh AS kpi 
     Inner JOIN du_an ON kpi.du_an_id = du_an.id 
     INNER JOIN don_vi ON du_an.donvi_id = don_vi.id 
@@ -31,16 +31,13 @@ else if (!empty($don_vi)){
 
     $statement = $db->prepare($query);
     $statement->execute();
-    if($statement->rowCount() >= 1) $kpi = $statement->fetchAll(PDO::FETCH_ASSOC|PDO::FETCH_GROUP);
+    if ($statement->rowCount() >= 1) $kpi = $statement->fetchAll(PDO::FETCH_ASSOC | PDO::FETCH_GROUP);
     $statement->closeCursor();
-    $myJSON = json_encode($kpi ?? '',JSON_UNESCAPED_UNICODE);
+    $myJSON = json_encode($kpi ?? '', JSON_UNESCAPED_UNICODE);
     echo $myJSON;
-
-    
-}
-else {
+} else {
     //Lấy kpi trung binh tat ca du an
-    $query="SELECT don_vi.khoi,don_vi.ten AS ten_don_vi,temp.ten_du_an,temp.diem,temp.thoigian
+    $query = "SELECT don_vi.khoi,don_vi.ten AS ten_don_vi,temp.ten_du_an,temp.diem,temp.thoigian
     FROM (
         SELECT du_an.ten AS ten_du_an, kpi_quytrinh.diem, kpi_quytrinh.thoigian, du_an.donvi_id AS don_vi_id
         FROM kpi_quytrinh
@@ -48,15 +45,13 @@ else {
         WHERE quy_trinh_id = 8
     ) AS temp
     INNER JOIN don_vi ON temp.don_vi_id = don_vi.id
-    ORDER BY don_vi.khoi";
+    ORDER BY don_vi.khoi, thoigian";
 
     $statement = $db->prepare($query);
     $statement->execute();
     /* Group values by the first column */
     $kpi = $statement->fetchAll(PDO::FETCH_ASSOC);
     $statement->closeCursor();
-    $myJSON = json_encode($kpi ?? '',JSON_UNESCAPED_UNICODE);
+    $myJSON = json_encode($kpi ?? '', JSON_UNESCAPED_UNICODE);
     echo $myJSON;
 }
-
-?>
